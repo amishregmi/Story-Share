3292bfe7d9a56543075bb078a9479f30a2a24e78
[amish] <aregmi@ramapo.edu>
2020-02-13 15:08:05 -0500

    completed summary generator script python

--- app/src/main/java/aregmi/ramapo/edu/storyshare/HomeFragment.java
+++ app/src/main/java/aregmi/ramapo/edu/storyshare/HomeFragment.java
@@ -1,19 +1,84 @@
 package aregmi.ramapo.edu.storyshare;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class HomeFragment extends Fragment{
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private View current;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, null);
+        current = inflater.inflate(R.layout.fragment_home, container, false);
+        Button upload_button = current.findViewById(R.id.upload_button);
+
+        upload_button.setOnClickListener(this);
+
+        return current;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.upload_button:
+                uploadFile();
+        }
+
+    }
+
+    private void uploadFile() {
+        AlertDialog.Builder load_file = new AlertDialog.Builder(getActivity());
+        load_file.setTitle("Select Story");
+
+        String saved_stories_dir = Environment.getExternalStorageDirectory().getAbsolutePath()+"/saved_stories";
+        System.out.println("SAVED STORIES DIR IS: "+ saved_stories_dir);
+
+        final File[] all_files = new File(saved_stories_dir).listFiles();
+        List<String> text_files = new ArrayList<String>();
+
+//        if (!(all_files.length == 0)){
+
+            for (File onefile: all_files){
+                String file_name = onefile.getName();
+                if (file_name.endsWith(".txt")){
+                    text_files.add(file_name);
+                }
+            }
+
+            int size = text_files.size();
+            String[] options = new String[size];
+            options = text_files.toArray(options);
+
+            load_file.setItems(options, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ListView options = ((AlertDialog)dialog).getListView();
+                    String selected_file = (String) options.getAdapter().getItem(which);
+                    System.out.println("Selected file is: "+ selected_file);
+
+                }
+            });
+
+            load_file.show();
+        //}
+
+
     }
 }
--- app/src/main/java/aregmi/ramapo/edu/storyshare/ProfileFragment.java
+++ app/src/main/java/aregmi/ramapo/edu/storyshare/ProfileFragment.java
@@ -13,8 +13,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +25,8 @@ import java.util.Map;
 public class ProfileFragment extends Fragment implements View.OnClickListener{
 
     private View current;
+    private EditText user_name;
+    private EditText user_description;
 
 
     @Nullable
@@ -29,12 +34,51 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         current = inflater.inflate(R.layout.activity_profile, container, false);
+        user_name = current.findViewById(R.id.user_name);
+        user_description = current.findViewById(R.id.user_description);
+        getCurrentProfileDetails();
         Button logout_button = current.findViewById(R.id.logout_button);
         Button save_button = current.findViewById(R.id.button_save_profile);
         logout_button.setOnClickListener(this);
         save_button.setOnClickListener(this);
 
         return current;
+    }
+
+    private void getCurrentProfileDetails() {
+        FirebaseAuth firebase_db_auth = FirebaseAuth.getInstance();
+        String current_user_id = firebase_db_auth.getCurrentUser().getUid();
+        DatabaseReference user_db_reference = FirebaseDatabase.getInstance().getReference().child("Users").child(current_user_id);
+
+        user_db_reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Check if data already exists
+
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() !=0 ){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                    if (map.get("user_name") != null){
+                        String current_name = map.get("user_name").toString();
+                        user_name.setText(current_name);
+                    }
+
+                    if (map.get("user_description") != null){
+                        String current_description = map.get("user_description").toString();
+                        user_description.setText(current_description);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
@@ -53,13 +97,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     }
 
     private void saveUserDetails() {
-        EditText user_name = current.findViewById(R.id.user_name);
         String user_name_extract = user_name.getText().toString();
-        EditText user_description = current.findViewById(R.id.user_description);
         String user_description_extract = user_description.getText().toString();
-
-        System.out.println("USER NAME IS: "+ user_name_extract);
-        System.out.println("USER DESCRIPTION IS: "+ user_description_extract);
 
         //DATABASE STUFF NOW
 
